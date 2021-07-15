60770ed91dd4e9c76168126d3d7a0ae1a2d6ce6e
saxenaharsh1000 <saxenaharsh1000@gmail.com>
2021-07-14 22:30:42 -0700

    Add node modules

--- app.js
+++ app.js
@@ -136,9 +136,9 @@ app.get("/about", function(req, res){
 });
 let port = process.env.PORT;
 if(port == null || port == "") {
-  port =8000;
+  port = 3000;
 }
-app.listen(port);
+
 app.listen(port, function() {
   console.log("Server has started Successfully.");
 });
